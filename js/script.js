496e4c6242ad23c412a2ac3494c56bba558e747a
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2024-12-02 23:52:47 -0600

    Test email link using EmailJS
    Test email link using EmailJS

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function sendMail(){
+    let parms = {
+        name : document.getElementById("name").value,
+        phone : document.getAnimations("phone_num").value,
+        email : document.getAnimations("email").value,
+        message : document.getAnimations("message").value,
+    }
+
+    emailjs.send("service_mc0hf6g", "template_d598vrk",parms).then(aler("Email Sent!"))
+}
