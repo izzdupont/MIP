1b82c507af76a891a72fe31d335d0c50705e5556
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2024-12-03 00:26:41 -0600

    Update script.js
    update error from email fetch script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function sendMail(){
     let parms = {
         name : document.getElementById("name").value,
-        phone : document.getAnimations("phone_num").value,
-        email : document.getAnimations("email").value,
-        message : document.getAnimations("message").value,
+        phone : document.getElementById("phone_num").value,
+        email : document.getElementById("email").value,
+        message : document.getElementById("message").value,
     }
 
     emailjs.send("service_mc0hf6g", "template_d598vrk",parms).then(alert("Email Sent!!"))
