b708f7d460d577ccbb14703eaae75da32966bd37
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2024-12-03 00:01:52 -0600

    Update script.js
    EmailJS test 2

--- js/script.js
+++ js/script.js
@@ -6,5 +6,5 @@ function sendMail(){
         message : document.getAnimations("message").value,
     }
 
-    emailjs.send("service_mc0hf6g", "template_d598vrk",parms).then(aler("Email Sent!"))
+    emailjs.send("service_mc0hf6g", "template_d598vrk",parms).then(alert("Email Sent!!"))
 }
