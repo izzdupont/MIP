fab57e09db50488cf080b8576a7c0eb02145461d
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2024-12-15 20:56:06 -0600

    Revert back emailJS
    Revert back to midwestitprovider@gmail.com

--- js/script.js
+++ js/script.js
@@ -6,9 +6,5 @@ function sendMail(){
         message : document.getElementById("message").value,
     }
 
-<<<<<<< HEAD
-    emailjs.send("service_w6ukd88", "template_2akvs5s",parms).then(alert("Email Sent!!"))
-=======
     emailjs.send("service_mc0hf6g", "template_d598vrk",parms).then(alert("Email Sent!!"))
->>>>>>> parent of ff60741 (Update EmailJS)
 }
