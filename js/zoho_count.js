54a3913f7a95f8354f1d17a1aa5eabd8bd0a36a3
Luiz DuPont <91236027+izzdupont@users.noreply.github.com>
2025-08-14 16:10:00 -0500

    Move Script on Main to their own js

---
+++ js/zoho_count.js
@@ -0,0 +1,14 @@
+var $zoho=$zoho || {};
+$zoho.salesiq = $zoho.salesiq || {
+  widgetcode:"siq9513a88eaa3cc9e8584bb18fb527a513032a5e28f741db5e41a420d27e15ac5f",
+  values:{},
+  ready:function(){}
+};
+var d=document;
+var s=d.createElement("script");
+s.type="text/javascript";
+s.id="zsiqscript";
+s.defer=true;
+s.src="https://salesiq.zoho.com/widget";
+var t=d.getElementsByTagName("script")[0];
+t.parentNode.insertBefore(s,t);
